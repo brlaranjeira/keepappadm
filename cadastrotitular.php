534df854edb3ce92b75b95800ae07f5157a06bea
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-09-10 18:42:04 -0300

    insere alguns campos nos formularios de cadastro de titular

--- cadastrotitular.php
+++ cadastrotitular.php
@@ -30,7 +30,6 @@
         </nav>
         <div class="tab-content pt-2" id="nav-tabContent">
             <div class="tab-pane fade show active" id="div-cadastro-individual" role="tabpanel" aria-labelledby="div-cadastro-individual-tab">
-            <!--<div id="div-cadastro-individual" class="mt-3 tab-pane fade show active" role="tabpanel" aria-labelledby="nav-cadastro-individual">-->
                 <h4>Cadastro de Titular</h4>
                 <div class="row">
                     <div class="col">
@@ -53,12 +52,54 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col"></div>
+                    <div class="col-lg-6 col-12">
+                        <label for="titular-genero">Gênero</label>
+                        <div class="input-group">
+                            <select class="form-control" name="titular-genero" id="titular-genero">
+                                <option value="f">Feminino</option>
+                                <option value="m">Masculino</option>
+                                <option value="o">Outro</option>
+                            </select>
+                            <div class="input-group-append"><span class="bg-info text-white input-group-text"><i class="fa fa-pencil-alt"></i></span></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <label for="titular-mail">E-mail</label>
+                        <div class="input-group">
+                            <input class="form-control" name="titular-mail" id="titular-mail" type="text">
+                            <div class="input-group-append"><span class="bg-info text-white input-group-text"><i class="fa fa-pencil-alt"></i></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-12">
+                        <label for="titular-celular">Telefone Celular</label>
+                        <div class="input-group">
+                            <input class="form-control" name="titular-celular" id="titular-celular" type="text">
+                            <div class="input-group-append"><span class="bg-info text-white input-group-text"><i class="fa fa-pencil-alt"></i></span></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <label for="titular-fone">Outro telefone</label>
+                        <div class="input-group">
+                            <input class="form-control" name="titular-fone" id="titular-fone" type="text">
+                            <div class="input-group-append"><span class="bg-info text-white input-group-text"><i class="fa fa-pencil-alt"></i></span></div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="tab-pane fade" id="div-cadastro-massivo" role="tabpanel" aria-labelledby="div-cadastro-massivo-tab">
-            <!--<div id="div-cadastro-massivo" class="mt-3 tab-pane fade" role="tabpanel" aria-labelledby="nav-cadastro-massivo">-->
                 <h4>Cadastro Massivo</h4>
+                <form enctype="multipart/form-data" action="./newusersfile.php">
+                    <div class="row">
+                        <div class="col">
+                            <div class="input-group">
+                                <input type="file" multiple>
+                                <div class="input-group-append"><span class="bg-primary text-white input-group-text">Enviar arquivo&nbsp;<i class="fa fa-cloud-upload-alt"></i></span></div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
